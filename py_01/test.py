706d5e198f83aebb66c2896afb34007e008cf32a
adrielemotos <dryty.am@gmail.com>
2025-07-18 15:25:09 -0300

    first commit

---
+++ py_01/test.py
@@ -0,0 +1,16 @@
+cookie ="gato preto e gordo"
+yaya ="gatinha fofa"
+
+print(cookie)
+print(yaya)
+
+a =5
+b =3
+print(a+b)
+
+quarto = 2
+
+if quarto == 2:
+    print("porta aberta")
+else:
+    print("porta fechada")
